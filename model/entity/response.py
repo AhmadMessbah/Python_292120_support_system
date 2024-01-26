a74c8f671f10ddb49e5a37e294b2b633f24459d1
Messbah.A <Messbah.A@Gmail.com>
2024-01-26 15:02:46 +0330

    Initial Commit - Messbah.A

---
+++ model/entity/response.py
@@ -0,0 +1,3 @@
+class response:
+    def __init__(self, ticket, response_text, date_time, user, status="Open"):
+        pass
---
+++ model/entity/ticket.py
@@ -0,0 +1,3 @@
+class Ticket:
+    def __init__(self, group, subject, description, date_time, user, status="Open"):
+        pass
