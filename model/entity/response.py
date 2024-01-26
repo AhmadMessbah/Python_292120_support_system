079099cbe8e6c8be693ca634730faca2f954783b
Student <alisoori991@gmail.com>
2024-01-26 16:39:54 +0330

    Entity Updated

--- model/entity/response.py
+++ model/entity/response.py
@@ -1,3 +1,8 @@
 class response:
     def __init__(self, ticket, response_text, date_time, user, status="Open"):
-        pass
+        self.id = None
+        self.ticket = ticket
+        self.response_text = response_text
+        self.date_time = date_time
+        self.user = user
+        self.status = status
--- model/entity/ticket.py
+++ model/entity/ticket.py
@@ -1,3 +1,9 @@
 class Ticket:
     def __init__(self, group, subject, description, date_time, user, status="Open"):
-        pass
+        self.id = None
+        self.group = group
+        self.subject = subject
+        self.description = description
+        self.date_time = date_time
+        self.user = user
+        self.status = status
