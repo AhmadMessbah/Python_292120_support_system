01147ab9325dd1830c12102d48ce5d5bbddba8d2
alif35a <ali.farzanian99@gmail.com>
2024-01-26 16:49:29 +0330

    Entity

--- model/entity/response.py
+++ model/entity/response.py
@@ -1,11 +1,6 @@
-class Response:
+class response:
     def __init__(self, ticket, response_text, date_time, user, status="Open"):
-        self.id = None
-        self.ticket = ticket
-        self.response_text = response_text
-        self.date_time = date_time
-        self.user = user
-        self.status = status
+        pass
 
     def __repr__(self):
         return str(self.__dict__)
--- model/entity/ticket.py
+++ model/entity/ticket.py
@@ -1,12 +1,6 @@
 class Ticket:
     def __init__(self, group, subject, description, date_time, user, status="Open"):
-        self.id = None
-        self.group = group
-        self.subject = subject
-        self.description = description
-        self.date_time = date_time
-        self.user = user
-        self.status = status
+        pass
 
     def __repr__(self):
         return str(self.__dict__)
