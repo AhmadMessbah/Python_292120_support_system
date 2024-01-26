235aa97549f206f0ebf00585fc66d3368b2e0bf3
alif35a <ali.farzanian99@gmail.com>
2024-01-26 18:55:46 +0330

    Sql Alchemy Added

--- model/entity/user.py
+++ model/entity/user.py
@@ -1,6 +1,16 @@
-class User:
+from sqlalchemy import Column, Integer, String, Boolean, DateTime
+
+from model.entity.base import Base
+
+
+class User(Base):
+    __tablename__ = "user_tbl"
+    id = Column(Integer, primary_key=True)
+    name = Column(String(30))
+
+    status = Column(Boolean)
+
     def __init__(self, name, family, username, password, role, status=True):
-        self.id = None
         self.name = name
         self.family = family
         self.username = username
