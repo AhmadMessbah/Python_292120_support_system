7b912ab70fd8e205da7bce336693c2676dd3fdd0
Student <alisoori991@gmail.com>
2024-01-26 16:46:47 +0330

    Entity Updated

--- model/entity/response.py
+++ model/entity/response.py
@@ -2,5 +2,6 @@ class response:
     def __init__(self, ticket, response_text, date_time, user, status="Open"):
         pass
 
+
     def __repr__(self):
         return str(self.__dict__)
--- model/entity/ticket.py
+++ model/entity/ticket.py
@@ -2,5 +2,6 @@ class Ticket:
     def __init__(self, group, subject, description, date_time, user, status="Open"):
         pass
 
+
     def __repr__(self):
         return str(self.__dict__)
